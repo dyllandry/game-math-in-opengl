a32e1719bc5e1e42ed5df14832fee553573a79ee
Dylan Landry <dylanglandry@gmail.com>
2021-10-21 08:31:49 -0400

    draw triangle on screen

--- src/main.c
+++ src/main.c
@@ -9,17 +9,27 @@ const char *vertexShaderSource = "#version 330 core \n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main()\n"
 	"{\n"
-	"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n";
 
 int main()
 {
+	/* glfw: init */
+	/* -------------------------------------------------------------------- */
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// create window
+	/* glfw: create window */
+	/* -------------------------------------------------------------------- */
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -27,68 +37,130 @@ int main()
 		glfwTerminate();
 		return 1;
 	}
-
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// load opengl function pointers w/ glad
+	/* glad: load opengl function pointers */
+	/* -------------------------------------------------------------------- */
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		printf("Failed to initialize GLAD\n");
 		return 1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	/* Build and compile shader program */
+	/* -------------------------------------------------------------------- */
+	/* Compile our vertex shader */
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	/* Check if vertex shader compiled correctly */
+	{
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+			return 1;
+		}
+	}
+	/* Compile our fragment shader. */
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	/* Check if fragment shader compiled correctly */
+	{
+		int success;
+		char infoLog[512];
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
+			return 1;
+		}
+	}
+	/* Links shaders into shader program. */
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	/* Check if shader program linked successfuly */
+	{
+		int success;
+		char infoLog[512];
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s", infoLog);
+			return 1;
+		}
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
-	// setup callback for handling window resizing
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	// Setup vertex data and vertex buffer object
+	/* Setup vertex data, buffers, and configure vertex attributes. */
+	/* -------------------------------------------------------------------- */
 	float vertices[] =
 	{
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f
 	};
-
-	unsigned int VBO;
+	/* Setup vertex buffer object, vertex array object */
+	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	/* Bind VAO first, then VBO, then setup vertex attributes. */
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	/* Link vertex attributes */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	/* Unbind VBO. */
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	/* Unbind VAO. */
+	glBindVertexArray(0);
 
-	// Compile our vertex shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	// Check if vertex shader compiled correctly
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
-		return 1;
-	}
-
-	// TODO: compile our fragment shader
-
+	/* uncomment this call to draw in wireframe polygons. */
+	/* glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); */	
+	
+	/* Render loop */
+	/* -------------------------------------------------------------------- */
 	while (!glfwWindowShouldClose(window))
 	{
-		// handle user input
+		/* handle user input */
 		processInput(window);
-
-		// rendering commands
+		/* render */
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		/* Draw triangle */
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glad_glDrawArrays(GL_TRIANGLES, 0, 3);
+		/* Don't have to unbind since we don't draw anything else, but */
+		/* you would if you had other shapes to draw. */
+		glBindVertexArray(0);
 
-		// check and call events and swap the double render  buffer
+		/* glfw: swap the double render buffer & poll IO events */
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	/* De-allocate resources */
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	/* glfw: terminate, clears all allocated GLFW resources */
 	glfwTerminate();
 	return 0;
 }
